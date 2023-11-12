f33233e795a7df188c4f918f791c82a196d5a82f
Ryosuke501 <r.leikid.tomodachi.24.925@gmail.com>
2023-11-12 12:39:06 +0900

    プレイタイムの型変更

--- dartsgames/models.py
+++ dartsgames/models.py
@@ -13,7 +13,7 @@ class Game_result(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.CASCADE, verbose_name= 'ユーザー名')
     game_name = models.ForeignKey(Game_master , on_delete=models.CASCADE)
     score = models.CharField(max_length=50, verbose_name= 'スコア')
-    play_time = models.DateTimeField(blank= True, null= True, verbose_name= '１ゲームにかかった時間')
+    play_time = models.IntegerField(blank= True, null= True, verbose_name= '１ゲームにかかった時間')
     create_at = models.DateTimeField(default= timezone.now, verbose_name= '更新日時')
     
     def __str__(self):
